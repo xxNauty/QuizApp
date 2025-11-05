bee4b5fbd8bfade5ef15b8d9511d1e3dfdd78ba7
Mateusz Wnuk(Laptop) <mateusz2003w@gmail.com>
2025-11-05 14:56:27 +0100

    reader for data in json format

--- data_reader/csv_reader.py
+++ data_reader/csv_reader.py
@@ -7,11 +7,12 @@ def read_file(filename: str, encoding: str = 'utf8') -> list[dict]|None:
     try:
         with open(filename, 'r', encoding=encoding) as file:
             questions = []
+
             csv_file = csv.DictReader(file)
             for line in csv_file:
                 questions.append(line)
 
-            logging.info("Questions read successfully from the %s file, there are %s questions", filename.split("/")[-1],  len(questions))
+            logging.info("Questions read successfully from the %s file(CSV), there are %s questions", filename.split("/")[-1],  len(questions))
             file.close()
             return questions
     except FileNotFoundError:
---
+++ data_reader/json_reader.py
@@ -0,0 +1,26 @@
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def read_file(filename: str, encoding: str = 'utf8') -> list[dict]|None:
+    try:
+        with open(filename, 'r', encoding=encoding) as file:
+            questions = []
+
+            data = json.load(file)
+            for question in data:
+                questions.append(question)
+
+            logging.info("Questions read successfully from the %s file(JSON), there are %s questions", filename.split("/")[-1],  len(questions))
+            file.close()
+            return questions
+    except FileNotFoundError:
+        logging.error("There is no such file as %s", filename)
+        return None
+
+
+# questions = read_file("../data/pozwolenie-na-posiadanie-broni-palnej.json")
+#
+# for question in questions:
+#     print(question)
