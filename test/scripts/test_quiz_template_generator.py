28a406396138824d18fcd77d83f40a1cea919484
Mateusz Wnuk(PC) <mateusz2003w@gmail.com>
2025-11-21 12:36:52 +0100

    temporary fix

--- test/scripts/test_quiz_template_generator.py
+++ test/scripts/test_quiz_template_generator.py
@@ -25,7 +25,7 @@ from scripts import quiz_template_generator
         ("second-quiz", "json", 100, 100)
     ]
 )
-def test_for_correct_data(isolated_cwd, monkeypatch, name, file_format, number_of_questions, minimum_to_pass):
+def test_for_correct_data(monkeypatch, name, file_format, number_of_questions, minimum_to_pass):
     inputs = iter([name, file_format, number_of_questions, minimum_to_pass])
     monkeypatch.setattr("builtins.input", lambda prompt="": next(inputs)) # symulowanie danych wprowadzonych przez użytkownika
 
@@ -47,7 +47,7 @@ def test_for_correct_data(isolated_cwd, monkeypatch, name, file_format, number_o
         ("First_quiz", ["HTML", "CSS", "", "dog", "Json"], 20, 12),
     ]
 )
-def test_for_incorrect_file_formats(isolated_cwd, monkeypatch, name, file_format, number_of_questions, minimum_to_pass):
+def test_for_incorrect_file_formats(monkeypatch, name, file_format, number_of_questions, minimum_to_pass):
     inputs = iter([name, *file_format, number_of_questions, minimum_to_pass]) # * przy file_format oznacza rozpakowywanie listy
     monkeypatch.setattr("builtins.input", lambda prompt="": next(inputs))
 
@@ -69,7 +69,7 @@ def test_for_incorrect_file_formats(isolated_cwd, monkeypatch, name, file_format
         ("First_quiz", "csv", 20, [22, 99, 12]),
     ]
 )
-def test_for_min_to_pass_greater_than_num_of_questions(isolated_cwd, monkeypatch, name, file_format, number_of_questions, minimum_to_pass):
+def test_for_min_to_pass_greater_than_num_of_questions(monkeypatch, name, file_format, number_of_questions, minimum_to_pass):
     inputs = iter([name, file_format, number_of_questions, *minimum_to_pass])
     monkeypatch.setattr("builtins.input", lambda prompt="": next(inputs))
 
