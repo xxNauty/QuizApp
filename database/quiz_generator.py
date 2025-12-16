a9025277ab1e9c43b1801a295f4e61e2ea1be3a2
Mateusz Wnuk(PC) <mateusz2003w@gmail.com>
2025-12-16 12:01:41 +0100

    random order of answers in question

--- database/quiz_generator.py
+++ database/quiz_generator.py
@@ -18,8 +18,28 @@ def generate_quiz(available_questions: list, number_of_questions: int) -> list[d
     logger.info("The question drawing begins")
     for id_of_questions in ids_of_questions:
         logger.info(f"Chosen question number {id_of_questions + 1}")
-        questions.append(available_questions[id_of_questions])
+        questions.append(_shuffle_answers(available_questions[id_of_questions]))
 
     logger.info(f"Quiz generated, there are {number_of_questions} questions")
 
     return questions
+
+def _shuffle_answers(question: dict) -> dict:
+    letters_of_answers = ['A', 'B', 'C']
+    answers = [
+        ["A", question['answer_a']],
+        ["B", question['answer_b']],
+        ["C", question['answer_c']]
+    ]
+    random.shuffle(answers)
+
+    question['answer_a'] = answers[0][1]
+    question['answer_b'] = answers[1][1]
+    question['answer_c'] = answers[2][1]
+
+    for i in range(3):
+        if answers[i][0] == question['correct']:
+            question['correct'] = letters_of_answers[i]
+            break
+
+    return question
